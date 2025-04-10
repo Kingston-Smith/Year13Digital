216ad37212192345ba0124d0ab04c29279b04f50
Kingston-Smith <163068313+Kingston-Smith@users.noreply.github.com>
2025-04-10 12:37:50 +1200

    Bug fix
    First bug fix
    Previous version: all auth routes opened the sign up page.
    Fix: Moving the definitions directly under their own routes

--- website/auth.py
+++ website/auth.py
@@ -4,11 +4,11 @@ from flask import Blueprint, render_template
 auth=Blueprint("auth", __name__)
 #routes, basically the code that tells it what the pages are, this section is for pages where users do stuff with their accounts
 @auth.route("/sign-up")
-@auth.route("/login")
-@auth.route("/account")
 def sign_up():
     return render_template("sign_up.html")
+@auth.route("/login")
 def login():
     return render_template("login.html")
+@auth.route("/account")
 def account():
     return render_template("account.html")
