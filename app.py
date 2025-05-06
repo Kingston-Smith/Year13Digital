3b97b7bc944d30b13d86eca9e88e6c6a3ea3179c
Kingston-Smith <163068313+Kingston-Smith@users.noreply.github.com>
2025-05-07 09:50:12 +1200

    Cleaning up commenting

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #This is the page which you run to actually run the website, it is outside the website in the file structure, don't move it, it's meant to be outside it
 #Additionally, don't add or remove anything from here
-#create_app is from __init__
+
+#Import create app fuction from  __init__.py
 from website import create_app
-#running the app
+
+#run create app function as main
 if __name__=="__main__":
     app=create_app()
     app.run(debug=True)
