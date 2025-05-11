86eb0ca3a43a1721a5ca10c464bf0de5cce3e724
Kingston-Smith <163068313+Kingston-Smith@users.noreply.github.com>
2025-05-12 11:51:45 +1200

    Finishing sign up route

--- website/auth.py
+++ website/auth.py
@@ -23,19 +23,30 @@ def sign_up():
         password1=request.form.get("password1")
         password2=request.form.get("password2")
 
-    email_exists=User.query.filter_by(email=email).first()
-    username_exists=User.query.filter_by(username=username).first()
+        email_exists=User.query.filter_by(email=email).first()
+        username_exists=User.query.filter_by(username=username).first()
 
-    if email_exists:
-        flash('email is taken', category='error')
-    elif username_exists:
-        flash('username is taken', category='error')
-    elif password1!=password2:
-        flash('Confirmation password does not match', category='error')
-    elif len(password1)<8:
-        flash('password must be at least 8 characters', category='error')
-    elif len(password1)>20:
-        flash('password can not be more than 20 characters', category='error')
+        if email_exists:
+            flash('email is taken', category='error')
+        elif username_exists:
+            flash('username is taken', category='error')
+        elif password1!=password2:
+            flash('Confirmation password does not match', category='error')
+        elif len(password1)<8:
+            flash('password must be at least 8 characters', category='error')
+        elif len(password1)>20:
+            flash('password can not be more than 20 characters', category='error')
+        elif len(username)<2:
+            flash('username must be at least 2 characters', category='error')
+        elif len(email)<4:
+            flash('Email is invalid', category='error')
+        else:
+            new_user=User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:3276832768:8:1'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('your account has been created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
     
