016d740fc8008e69e28b1f8cc4a680857fdc9289
Kingston-Smith <163068313+Kingston-Smith@users.noreply.github.com>
2025-05-12 12:06:08 +1200

    Bug fix
    Sign up didn't work properly because i copied the sign up hash wrong

--- website/auth.py
+++ website/auth.py
@@ -41,7 +41,7 @@ def sign_up():
         elif len(email)<4:
             flash('Email is invalid', category='error')
         else:
-            new_user=User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:3276832768:8:1'))
+            new_user=User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
