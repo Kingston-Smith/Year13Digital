853bba87f8473688883af1b2931883fa8ea8eb0f
Kingston-Smith <163068313+Kingston-Smith@users.noreply.github.com>
2025-06-19 13:26:39 +1200

    Formating
    Just formatting the Update post page.

--- website/forms.py
+++ website/forms.py
@@ -10,4 +10,4 @@ from .models import User
 class PostForm(FlaskForm):
     title=StringField('Title', validators=[DataRequired()])
     content=TextAreaField('Content', validators=[DataRequired()])
-    submit=SubmitField('Post')
+    submit=SubmitField('Update')
